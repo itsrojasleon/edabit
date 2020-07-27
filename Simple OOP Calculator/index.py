be71481d320f9556f6bbefb7301e10395dc3c4cd
Juan Luis Rojas León <rojasleon122@gmail.com>
2020-07-27 14:35:01 -0500

    Simple OOP Calculator

---
+++ Simple OOP Calculator/index.py
@@ -0,0 +1,12 @@
+# Simple OOP Calculator
+# https://edabit.com/challenge/ta8GBizBNbRGo5iC6
+
+class Calculator:	
+	def add(self, a, b):
+		return a + b
+	def subtract(self, a, b):
+		return a - b
+	def multiply(self, a, b):
+		return a * b
+	def divide(self, a, b):
+		return a / b
